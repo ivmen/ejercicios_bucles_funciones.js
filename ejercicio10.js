2b0a20b19b8dfafb64e0209d77cd00093e3f9a5e
ivmen <ivan.mendez.pacheco@gmail.com>
2024-10-03 20:33:52 +0200

    Update ejercicio10.js
    Revisión del ejercicio 10

--- ejercicio10.js
+++ ejercicio10.js
@@ -9,46 +9,21 @@ const capitals = {
     Austria: 'Vienna',
     Hungary: 'Budapest',
     Ireland: 'Dublin'
-  };
-
-
-  function countryCreator (country, capital){
-    this.country = country;
-    this.capital = capital;  
- }
-
-function objectCreate (list){
-    let capitalsKeys = Object.entries(list);
-    let newCapitals = []
-    for (let i = 0; i < capitalsKeys.length; i++){
-        newCapitals.push(new countryCreator(capitalsKeys[i][0], capitalsKeys[i][1]));
-    }
-    return newCapitals;
-}
-
-function comparator(ciudad, list){
-    let respuesta = false;
-    let capitales = objectCreate(list);
-    let city = "";
-    let capital = "";
-    for (const busqueda of capitales) {
-        if (busqueda.country == ciudad){
-            city = busqueda.country;
-            capital = busqueda.capital;
-            respuesta = true;
-        }
-    }
-    if (respuesta == true){
-        console.log("La capital de " + city + " es " + capital)
-    }else{
-        console.log(city + " no esta en nuestra base de datos");
-    }
-}
+};
 
 function getCapital(country) {
-    comparator(country, capitals);
+    let noExiste = false;
+    for (const key in capitals){
+        if (key === country){
+            console.log("La capital de " + country + " es " + capitals[key])
+            noExiste = true;
+        }
+    }
+    if (!noExiste){
+        console.log(country + " no esta en nuestra base de datos");
+    }
 }
-  
-getCapital("Portugal");
+
+getCapital("Spain");
 
   
